3e1e8f6d7d127d3c76889d59a86c1823b380d8b3
Error1000 <udreadanmihai@gmail.com>
2021-04-15 23:35:52 +0300

    Improve example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,62 +1,55 @@
 extern crate one_user;
-
-
 #[macro_use]
 extern crate lazy_static;
 
 mod test{
-
-use one_user::one_user;
-#[one_user]
-#[derive(Debug)]
-pub struct Test2 {
-    item: usize
-}
-
-
-impl Test2{
-    fn new(d: usize) -> UnboundTest2{
-        UnboundTest2::from(Test2{ item: d })
+    use one_user::one_user;
+    #[one_user]
+    #[derive(Debug)]
+    pub struct Test {
+        item: usize
     }
-}
 
-impl test2_binder::OnBind for Test2 {
-    fn on_bind<const SLOT: usize>(&mut self) {}
-}
+
+    impl Test{
+        pub fn new(d: usize) -> UnboundTest{
+            UnboundTest::from(Test{ item: d })
+        }
+    }
+
+    impl test_binder::OnBind for Test {
+        fn on_bind<const SLOT: usize>(&mut self) {}
+    }
 
 }
 
 fn main() {
-    let mut b = test::Test2Bouncer::new();
-    // let v = test::Test2::default();
+    let mut b = test::TestBouncer::new();
+    let mut b2 = test::TestBouncer::new(); // Can't create more than 1 bouncer if no arguments are specified to the macro
 
-   // let mut t1 = Test2::default();
-   // let mut t2 = Test2::default();
+    let mut t1 = test::Test::new(1);
+    let mut t2 = test::Test::new(2);
+    // Here we cannot use t1 or t2 because they are not bound
+    //println!("{:?}", t1); // Error
 
-    //let t1 = t1.bind(&mut b);
-   // println!("{:?}", t1);
+    // This works:
+    
+    let bound_t1 = t1.bind(&mut b);
+    println!("{:?}", *bound_t1); // Now we can use t1
+    
+    let bound_t2 = t2.bind(&mut b);
+    println!("{:?}", *bound_t2); // Now we can use t2
 
-    //let t2 = t2.bind(&mut b);
-   // println!("{:?}", t2);
 
-    /*
-    let mut bounce0: BOUNCER = BOUNCER::new().unwrap();
-    /*
-        println!("Size of BOUNCE<{}>: {} bytes", 0, std::mem::size_of::<BOUNCER<0>>());
-        println!("Size of Mutex<[bool; {}]>: {} bytes", 2, std::mem::size_of::<Mutex<[bool; 2]>>());
-        println!("Size of multi_bind::Unbound: {} bytes", std::mem::size_of::<multi_test::Unbound>());
-        println!("Size of Test: {} bytes", std::mem::size_of::<Test>());
-        println!("Size of multi_bind::Bound<{}>: {} bytes", 1, std::mem::size_of::<multi_test::Bound<1>>());
-    */
+    // This dosen't:
+/*
+    let bound_t1 = t1.bind(&mut b);
+    let bound_t2 = t2.bind(&mut b); // Error: Can't have more than one user at a time, a.k.a you can only have access to one instance of the Test struct at a time
 
-    println!();
-    let mut inst = Test::new(2);
-    let mut inst2 = Test::new(3);
 
-    let inst2 = inst2.bind(&mut bounce0);
-    println!("Hi: {:?}!", *inst2);
+    println!("{:?}", *bound_t1); // Now we can use t1
+    
+    println!("{:?}", *bound_t2); // Now we can use t2
+*/
 
-    let inst = inst.bind(&mut bounce0);
-
-    println!("Hi: {:?}!", *inst); */
 }
